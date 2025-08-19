1c0d8acbe20dbc261506863852e0c50710f2e035
Andrea To Chip <andreatochip@yahoo.com>
2025-08-19 16:56:37 +0800

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,16 @@
-const number = document.getElementByIdAll(".number");
-const display = document.getElementById("#display");
+const number = document.getElementsByClassName("number");
+const operation = document.getElementsByClassName("operation");
+const display = document.getElementById("display");
 display.textContent = "";
 display.style.color = "white";
+
+https://www.w3schools.com/js/js_htmldom_document.asp
+
+display.removeChild(text)
+display.appendChild(text)
+
+document.getElementById(id).onclick = function(){code}
+
+element.innerHTML =  new html 
+element.attribute = new value	
+element.style.property = new style
