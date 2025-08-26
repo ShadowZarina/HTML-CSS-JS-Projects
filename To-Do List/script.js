4b4b4a2a8ae778552e60ac26b04e98cfff59d6ae
Andrea To Chip <andreatochip@yahoo.com>
2025-08-26 16:21:13 +0800

    Create script.js

---
+++ To-Do List/script.js
@@ -0,0 +1,8 @@
+    const myButton = document.getElementById('myButtonId'); // If your button has an ID
+    // or
+    const addTaskItem = document.querySelector('.add-task'); // If your button has a class
+
+    addTaskItem.addEventListener('click', function() {
+        // Code to execute when the button is clicked
+        alert('Button clicked!');
+    });
