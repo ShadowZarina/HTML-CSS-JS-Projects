4e4c4b9eaec111f71521c9c94f4d0bbc29972989
Andrea To Chip <andreatochip@yahoo.com>
2025-09-04 16:43:41 +0800

    Update script.js

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,11 +1,13 @@
-    const myButton = document.getElementById('myButtonId'); // If your button has an ID
-    // or
-    const addTaskItem = document.querySelector('.add-task'); // If your button has a class
 
-    addTaskItem.addEventListener('click', function() {
-        // Code to execute when the button is clicked
-        alert('Button clicked!');
-    });
+
+const myButton = document.getElementById('myButtonId'); // If your button has an ID
+
+const addTaskItem = document.querySelector('.add-task'); // If your button has a class
+
+addTaskItem.addEventListener('click', function() {
+    // Code to execute when the button is clicked
+    alert('Button clicked!');
+});
 
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
