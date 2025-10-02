929a86ed2408a1a513c34300081bfcc4166d9625
Andrea To Chip <andreatochip@yahoo.com>
2025-10-02 09:36:22 +0800

    Create main.js

---
+++ Loading Screen/main.js
@@ -0,0 +1,18 @@
+const text = document.getElementById("text-element");
+
+const textArray = [
+            "Sharing your code with the world!",
+            "Creating another repository!",
+            "Pushing a new commit!",
+            "Generating a fresh experience!"
+        ];
+        let currentIndex = 0;
+
+        function changeText() {
+            text-element.textContent = textArray[currentIndex];
+            currentIndex = (currentIndex + 1) % textArray.length;
+        }
+
+        changeText(); 
+
+        setInterval(changeText, 3000); 
